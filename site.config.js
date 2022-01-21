3c3b1fbca75f0f79b60f60725d6e6fe7bc1daae5
hayoung0Lee <hayoung0.lee@gmail.com>
2022-01-21 14:57:52 +0900

    update twitter account

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '0cd7f1acfcdd445b9a82794a7cc5e062',
+  rootNotionPageId: "0cd7f1acfcdd445b9a82794a7cc5e062",
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Hayoung Lee',
-  domain: 'https://notion-blog-murex-one.vercel.app/',
-  author: 'Hayoung Lee',
+  name: "Hayoung Lee",
+  domain: "https://notion-blog-murex-one.vercel.app/",
+  author: "Hayoung Lee",
 
   // open graph metadata (optional)
-  description: 'Hayoung Tech-log',
-  socialImageTitle: 'Hayoung Lee',
-  socialImageSubtitle: 'Hello World! 👋',
+  description: "Hayoung Tech-log",
+  socialImageTitle: "Hayoung Lee",
+  socialImageSubtitle: "Hello World! 👋",
 
   // social usernames (optional)
-  twitter: 'futurerichasian',
-  github: 'hayoung0Lee',
-  linkedin: 'hayoung-lee-42808815a',
+  twitter: "hylee_dev",
+  github: "hayoung0Lee",
+  linkedin: "hayoung-lee-42808815a",
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
@@ -47,5 +47,5 @@ module.exports = {
   //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
   //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
   // }
-  pageUrlOverrides: null
-}
+  pageUrlOverrides: null,
+};
